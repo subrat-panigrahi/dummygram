789e7a191bbb6cb846a99439772dcfa8db0d0886
Subrat Panigrahi <subrat.panigrahi@paytmmall.com>
2022-04-17 20:54:24 +0530

    fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'build','static')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
